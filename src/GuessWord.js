35ef7af3740d2d8f241ba8d5774bda92ae209169
Miguel Sandoval <miguelship3@gmail.com>
2021-05-03 12:15:13 +0200

    Added tests for guessed words table

--- src/GuessWord.js
+++ src/GuessWord.js
@@ -7,7 +7,30 @@ const GuessWord = props => {
 		contents = (
 			<span data-test='guess-instructions'>Try to guess the word.</span>
 		);
-	}
+	} else {
+        const guessRow = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                        <tbody>
+                            { guessRow }
+                        </tbody>
+                    </thead>
+                </table>
+            </div>
+        );
+    }
 	return <div data-test='component-guessed-words'>{contents}</div>;
 };
 
