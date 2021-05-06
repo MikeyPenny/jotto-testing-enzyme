3624610ec8f9654b61212ccd1f7afbae5d9e0b55
Miguel Sandoval <miguelship3@gmail.com>
2021-05-06 21:58:42 +0200

    Added test for input on change the value

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Input = props => {
-	return <div data-test='input-comp'></div>;
+const Input = ({ secretWord }) => {
+	const [currentGuess, setCurrentGuess] = React.useState('');
+
+	const guessChangeHandler = ev => {
+		setCurrentGuess(ev.target.value);
+	};
+
+	return (
+		<div data-test='input-comp'>
+			<form data-test='form-inline'>
+				<input
+					type='text'
+					data-test='input-box'
+					className='mb-2 mx-sm-3'
+					placeholder='Enter Guess'
+					value={currentGuess}
+					onChange={ev => guessChangeHandler(ev)}
+				/>
+				<button
+					data-test='submit-button'
+					className='btn btn-primary mb-2'
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
+
+Input.propTypes = {
+	secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Input from './Input';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
-const setup = (props = {}) => {
-	return shallow(<Input />);
+const setup = (secretWord = 'nelson') => {
+	return shallow(<Input secretWord={secretWord} />);
 };
 
 test('renders without errors', () => {
 	const wrapper = setup();
 	const component = findByTestAttr(wrapper, 'input-comp');
 	expect(component.length).toBe(1);
+});
+
+
+test('does not warning with expected props', () => {
+	checkProps(Input, { secretWord: 'secret' });
+});
+
+// Test useState
+// Create a function to mock useState
+
+describe('state controlled input field', () => {
+	test('updates when input changed', () => {
+		const mockSetCurrentGuess = jest.fn();
+		React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+		const wrapper = setup();
+		const input = findByTestAttr(wrapper, 'input-box');
+		const mockEvent = { target: { value: 'train' } };
+		input.simulate('change', mockEvent);
+
+		expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+	});
 });
