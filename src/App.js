2eb9b81aa3b1422c25b5300c43103d0216460650
Miguel Sandoval <miguelship3@gmail.com>
2021-05-17 11:01:37 +0200

    Added basis to app component for testing

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Congrats from "./Congrats";
 import GuessWord from "./GuessWord";
 
 function App() {
-  return <div className='container'>
-    <h1>Jotto</h1>
-    <Congrats success={true} />
-    <GuessWord guessedWords={[ {guessedWord: 'train', letterMatchCount: 3} ]} />
-  </div>;
+  return (
+		<div data-test='component-app' className='container'>
+			<h1>Jotto</h1>
+			<Congrats success={true} />
+			<GuessWord
+				guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+			/>
+		</div>
+  );
 }
 
 export default App;
