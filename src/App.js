4c008ea94d4a3edd1a9bfbee845d52ead1c37914
Miguel Sandoval <miguelship3@gmail.com>
2021-05-18 10:51:33 +0200

    Added functional tests for guess word

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
 import './App.css';
+import Input from './Input';
 
 import Congrats from "./Congrats";
 import GuessWord from "./GuessWord";
 
 function App() {
+
+  // TODO: get props from shared state
+  const success = false;
+  const secretWord = 'party';
+  const guessedWords = [];  
+
   return (
 		<div data-test='component-app' className='container'>
 			<h1>Jotto</h1>
-			<Congrats success={true} />
+			<Congrats success={success} />
+      <Input success={success} secretWord={secretWord} />
 			<GuessWord
-				guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+				guessedWords={guessedWords}
 			/>
 		</div>
   );
