78d3a2e2e2494360c9fe902030a0a5d92e99131d
Miguel Sandoval <miguelship3@gmail.com>
2021-06-07 11:49:59 +0200

    Base project complete to start either redux or context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-
+import {useEffect} from 'react';
 import './App.css';
 import Input from './Input';
 
 import Congrats from "./Congrats";
 import GuessWord from "./GuessWord";
+import {getSecretWord} from './actions';
 
 function App() {
 
   // TODO: get props from shared state
   const success = false;
   const secretWord = 'party';
-  const guessedWords = [];  
+  const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, [])
 
   return (
 		<div data-test='component-app' className='container'>
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,38 @@
 import App from './App';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
+import { getSecretWord as mockGetSecretWord } from './actions';
+
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
 
 const setup = () => {
-	return shallow(<App />);
+	// Use mount because useEffect is not called on shallow
+	return mount(<App />);
 };;
 
 test('renders without error', () => {
 	const wrapper = setup();
 	const app = findByTestAttr(wrapper, 'component-app');
 	expect(app).toHaveLength(1);
+});
+
+
+describe('get secret word', () => {
+	beforeEach(() => {
+		mockGetSecretWord.mockClear();
+	});
+	test('getSecretWord on app mount', () => {
+		const wrapper = setup();
+
+		expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+	});
+
+	test('getSecretWord does not run on app update', () => {
+		const wrapper = setup();
+		mockGetSecretWord.mockClear();
+		// using setProps because wrapper.update() doesn't trigger useEffect
+		wrapper.setProps();
+		expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+	});
 });
